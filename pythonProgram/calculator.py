583e4ae0c6def66587a753dc479b8a8c3f6b51dd
VictorKPetersen <victorkahlpetersen@gmail.com>
2021-11-09 14:20:25 +0100

    Statistics

--- pythonProgram/calculator.py
+++ pythonProgram/calculator.py
@@ -1,3 +1,53 @@
-class calculator:
+from math import pow, sqrt
+from statistics import mode
 
-    def __init__(self):
+class Calculator:
+    minNumber = float()
+    maxNumber = float()
+    mostCommonNumber = float()
+    averageNumber = float()
+    varians = float()
+    spread = float()
+
+    def __init__(self, numList):
+        self.numList = numList
+
+        self.sortList()
+        self.findMinMax()
+        self.findMostCommon()
+        self.findAverage()
+        self.findVarians()
+        self.findSpread()
+
+
+    def sortList(self):
+        return self.numList.sort()
+
+    def findMinMax(self):
+        self.minNumber = self.numList[0]
+        self.maxNumber = self.numList[len(self.numList)-1]
+
+    def findMostCommon(self):
+        self.mostCommonNumber = (mode(self.numList))
+
+    def findAverage(self):
+        tempTotal = float()
+        for i in self.numList:
+            tempTotal = tempTotal + i
+        self.averageNumber = tempTotal/len(self.numList)
+
+
+    def findVarians(self):
+        tempNumber = float()
+        for i in self.numList:
+            tempNumber = tempNumber + pow((i - self.averageNumber), 2)
+        self.varians = tempNumber/len(self.numList)
+
+
+    def findSpread(self):
+        self.spread = sqrt(self.varians)
+
+
+
+
+
--- pythonProgram/main.py
+++ pythonProgram/main.py
@@ -1,24 +1,16 @@
-def findAverage(list):
-    totalEmuration = int()
-    average = int()
-    for num in list:
-        totalEmuration = num + totalEmuration
-        average = totalEmuration/len(list)
-    return average
-
-
-def findVariation():
-    print("")
-
-
-
-def findSpread():
-    print("")
-
+from calculator import Calculator
 
 def main():
-    numList = [1, 2, 3, 4, 5]
-    findAverage(numList)
+    numbersList = [-10, 7, -4, 8, -7, -10, 3, 5, 8, 3, -4, 2, -5, 1, 8, -11, 6, 6, -9, 5, -12,-10, 1, -7, -10, 2, -7, -11]
+
+    calc1 = Calculator(numbersList)
+
+    print(f"Smallest number is {calc1.minNumber} \nLargest number is {calc1.maxNumber}")
+    print(f"The most common number is {calc1.mostCommonNumber}")
+    print(f"The average number is {calc1.averageNumber}")
+    print(f"The varians is {calc1.varians}")
+    print(f"the spread is {calc1.spread}")
+
 
 
 if __name__ == '__main__':
