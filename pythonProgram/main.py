f5590aa2e1b5ea7c3b53291d503183534fd7404e
VictorKPetersen <victorkahlpetersen@gmail.com>
2021-11-09 13:05:12 +0100

    Classes

--- pythonProgram/main.py
+++ pythonProgram/main.py
@@ -1,4 +1,25 @@
+def findAverage(list):
+    totalEmuration = int()
+    average = int()
+    for num in list:
+        totalEmuration = num + totalEmuration
+        average = totalEmuration/len(list)
+    return average
+
+
+def findVariation():
+    print("")
+
+
+
+def findSpread():
+    print("")
+
+
 def main():
+    numList = [1, 2, 3, 4, 5]
+    findAverage(numList)
+
 
 if __name__ == '__main__':
     main()
