e20626e6cb147fbb9c505ccbaa2463a29d629e46
VictorKPetersen <83222801+VictorKPetersen@users.noreply.github.com>
2021-11-11 11:07:44 +0100

    More things

--- pythonProgram/calculator.py
+++ pythonProgram/calculator.py
@@ -5,49 +5,59 @@ class Calculator:
 
     def __init__(self, numList):
         self.numList = numList
-
-        self.minNumber = float()
-        self.maxNUmber = float()
-        self.mostCommonNumber = float()
-        self.averageNumber = float()
-        self.varians = float()
-        self.spread = float()
-        self.observations = len(numList)
-
         self.sortList()
-        self.findMinMax()
-        self.findMostCommon()
-        self.findAverage()
-        self.findVarians()
-        self.findSpread()
+
+        self.minNumber = self.findMin()
+        self.maxNumber = self.findMax()
+        self.diffrenceNumber = self.findDiffrence()
+        self.mostCommonNumber = self.findMostCommon()
+        self.averageNumber = self.findAverage()
+        self.varians = self.findVarians()
+        self.spread = self.findSpread()
+        self.observations = len(numList)
 
 
     def sortList(self):
         return self.numList.sort()
 
-    def findMinMax(self):
-        self.minNumber = self.numList[0]
-        self.maxNumber = self.numList[len(self.numList)-1]
+    def findMin(self):
+        return self.numList[0]
+
+
+    def findMax(self):
+        return self.numList[len(self.numList)-1]
 
     def findMostCommon(self):
-        self.mostCommonNumber = (mode(self.numList))
+        return (mode(self.numList))
+
+    def findDiffrence(self):
+        return self.maxNumber - self.minNumber
 
     def findAverage(self):
         tempTotal = float()
         for i in self.numList:
             tempTotal = tempTotal + i
-        self.averageNumber = tempTotal/len(self.numList)
+        return tempTotal/len(self.numList)
 
 
     def findVarians(self):
         tempNumber = float()
         for i in self.numList:
             tempNumber = tempNumber + pow((i - self.averageNumber), 2)
-        self.varians = tempNumber/len(self.numList)
+        return tempNumber/len(self.numList)
 
 
     def findSpread(self):
-        self.spread = sqrt(self.varians)
+        return sqrt(self.varians)
+
+    def printData(self):
+        print(f"Smallest number is {self.minNumber} \nLargest number is {self.maxNumber}")
+        print(f"There is a diffrence of {self.diffrenceNumber} between the smallest and largest number")
+        print(f"The most common number is {self.mostCommonNumber}")
+        print(f"There are {self.observations} observations")
+        print(f"The average number is {self.averageNumber}")
+        print(f"The varians is {self.varians}")
+        print(f"the spread is {self.spread}")
 
 
 
--- pythonProgram/main.py
+++ pythonProgram/main.py
@@ -4,13 +4,7 @@ def main():
     numbersList = [-10, 7, -4, 8, -7, -10, 3, 5, 8, 3, -4, 2, -5, 1, 8, -11, 6, 6, -9, 5, -12,-10, 1, -7, -10, 2, -7, -11]
 
     calc1 = Calculator(numbersList)
-
-    print(f"Smallest number is {calc1.minNumber} \nLargest number is {calc1.maxNumber}")
-    print(f"The most common number is {calc1.mostCommonNumber}")
-    print(f"There are {calc1.observations} observations")
-    print(f"The average number is {calc1.averageNumber}")
-    print(f"The varians is {calc1.varians}")
-    print(f"the spread is {calc1.spread}")
+    calc1.printData()
 
 
 
